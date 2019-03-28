6c4bc1bc48e126f4ee1ab7113f2945ad1840aaf2
Lmint <jeonkyosin@gmail.com>
2019-03-29 01:33:35 +0700

    Update mongodb link

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const mongo = {
   db_real:
-    'mongodb+srv://lmint:Taokobiet123@dfo-data-entry-f2qo2.gcp.mongodb.net/storage?retryWrites=true',
+    // 'mongodb+srv://lmint:Taokobiet123@dfo-data-entry-f2qo2.gcp.mongodb.net/storage?retryWrites=true',
+    'mongodb+srv://lmint:Taokobiet123@cluster0-l9cgs.gcp.mongodb.net/Devorum?retryWrites=true',
   db_test: 'mongodb://localhost/sheet_tool',
   options: {
     useNewUrlParser: true,
