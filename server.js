7219e89a7c0cafd434b5befdbb86a99568317741
Lmint <jeonkyosin@gmail.com>
2019-03-29 16:18:03 +0700

    Recover run only nodejs server

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const cors = require('cors')
 
 app.use(cors())
 
-// // Express Config
-app.use(express.static(path.join(__dirname, 'build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// // // Express Config
+// app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) => res.send('Server is running!'))
 
 // SETUP: Body-Parser
 const { json, urlencoded } = require('body-parser')
