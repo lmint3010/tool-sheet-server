b49fea8c68075fdb40e29c9113a89597fc16fb46
Lmint <jeonkyosin@gmail.com>
2019-04-08 14:59:35 +0700

    Fix search issue

--- controllers/api/tools/search.js
+++ controllers/api/tools/search.js
@@ -4,19 +4,23 @@ const isEmpty = require('../../../validation/isEmpty')
 
 const search = async (req, res) => {
   const { content, code } = req.body
+  if (code.length > 2) return res.json({ status: false })
 
-  const enzimeRegExp = new RegExp(strEnzime(content.toLowerCase()), 'g')
+  const enzimeRegExp = new RegExp(strEnzime(content.toLowerCase()))
 
   // Find all english docs match search case
   const results = await translation_model.find({ enzime: enzimeRegExp }).lean()
 
   if (isEmpty(results)) return res.json({ status: false })
 
-  const searchResults = results.map(({ site, sheet, text, translated }) => ({
-    text,
-    position: `${site} | ${sheet}`,
-    translated: translated[code.toLowerCase()],
-  }))
+  const searchResults = results
+    .reduce((arr, doc) => {
+      return (!doc.translated || !doc.translated[code]) ? arr : arr.concat({
+        text: doc.text,
+        position: `${doc.site} | ${doc.sheet}`,
+        translated: doc.translated[code.toLowerCase()],
+      })
+    }, [])
 
   res.json({ status: true, searchResults })
 }
